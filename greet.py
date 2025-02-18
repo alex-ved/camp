f0db8dd0a831adff64c4966267305b623610b563
Alexey Vedishchev <valexey1@gmail.com>
2025-02-18 07:52:13 +0100

    connect modules

--- greet.py
+++ greet.py
@@ -1,3 +1,4 @@
 def greet(name):
-    print("Hello", name)
+    print("Hello", name,'!')
+    print("Here is what you need to buy:")
     return 0
--- printShoppingList.py
+++ printShoppingList.py
@@ -1,3 +1,5 @@
+from greet import greet
+greet("John")
 with open('Shopping List.txt') as f:
     for item in f:
         print(item, end="")
